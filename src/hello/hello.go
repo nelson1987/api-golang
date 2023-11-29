3cb4ef7f4a1546139f45f5d812ea216105a60dfc
Nelson Neto <nelson.ash@outlook.com>
2023-11-29 16:08:59 +0000

    monitoramento de sites

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,10 +1,31 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"time"
+)
 
 func main() {
-	//site := "https://www.alura.com.br"
-	//resp, _ := http.Get(site)
-	//fmt.Println(resp)
-	fmt.Println("respo")
+	var sites [4]string
+	sites[0] = "https://random-status-code.herokuapp.com/"
+	sites[1] = "https://www.alura.com.br"
+	sites[2] = "https://www.caelum.com.br"
+
+	for i := 0; i < 5; i++ {
+		for i, site := range sites {
+			fmt.Println("Testando site", i, ":", site)
+			testaSite(site)
+		}
+		time.Sleep(20 * time.Second)
+	}
+}
+
+func testaSite(site string) {
+	resp, err := http.Get(site)
+	if resp.StatusCode == 200 {
+		fmt.Println(resp)
+	} else {
+		fmt.Println(err)
+	}
 }
